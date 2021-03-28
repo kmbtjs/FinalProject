1f3b1ef333d9011e3cbce1306b989df865f42199
KAMER BERAT TOPAL <ktopal18@posta.pau.edu.tr>
2021-03-28 22:27:18 +0300

    Some changes are done. Results folder added with proper error and success messages.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -25,10 +25,23 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             ProductManager productManager1 = new ProductManager(new EfProductDal());
-            foreach (var product in productManager1.GetProductDetails())
+
+            var result = productManager1.GetProductDetails();
+
+            if (result.Success == true)
             {
-                Console.WriteLine("Product Name: {0} Product Category: {1} ", product.ProductName, product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine("Product Name: {0} Product Category: {1} ", product.ProductName, product.CategoryName);
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+            
         }
+        
     }
 }
